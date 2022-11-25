193b842f09ef401197acbd351738b94c3f635326
Curtis Slone <curtisdslone@gmail.com>
2022-11-25 18:30:37 +0100

    TLS Cert test

--- SecureSoftwareApp/app.py
+++ SecureSoftwareApp/app.py
@@ -6,6 +6,8 @@ Description: REST app that allows x509 authenticated users to view temperature a
 and allows authenticated administrators to update sensor information
 """
 from flask import Flask, render_template, request, jsonify, redirect, url_for
+import ssl
+from werkzeug import serving
 app = Flask(__name__)
 
 #################
@@ -14,4 +16,10 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     """Return Homepage"""
-    return render_template('home.html')
+    return "hello"
+
+context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+context.load_cert_chain("simple.org.crt","simple.org.key")
+
+
+serving.run_simple("0.0.0.0", 8000, app, ssl_context=context)
