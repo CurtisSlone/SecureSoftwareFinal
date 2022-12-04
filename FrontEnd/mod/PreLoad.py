0dd7cf062bd341f3309c6178c2d6dd7c32031c1b
Curtis Slone <curtisdslone@gmail.com>
2022-12-04 09:39:14 +0100

    Preload available users

---
+++ FrontEnd/mod/PreLoad.py
@@ -0,0 +1,27 @@
+import os
+
+class PreLoad:
+    """
+    Preload available cert/key pairs for FrontEnd
+    """
+    def __init__(self):
+        """
+        Init Preloaded value
+        """
+        self.__dnListing = [
+            {
+                "dn": "fred-adm",
+                "certpath": "../TestCerts/User/fred-adm.crt",
+                "keypath": "../TestCerts/User/fred-adm.key"
+            },
+            {
+                "dn": "fred-user",
+                "certpath": "../TestCerts/User/fred-user.crt",
+                "keypath": "../TestCerts/User/fred-user.key"
+            }
+        ]
+    def getDNListing(self):
+        """
+        Publicly expose dnListing
+        """
+        return self.__dnListing    
